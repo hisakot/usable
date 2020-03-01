f895e7151a405ae040841aa14570de160c8c099a
hisakot <sakigake.apple@gmail.com>
2020-03-01 21:12:41 +0900

    simple mirror 16:9

--- webcam.py
+++ webcam.py
@@ -3,6 +3,8 @@ import cv2
 capture = cv2.VideoCapture(0)
 if not capture.isOpened():
     exit()
+capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
 while True:
     ret, frame = capture.read()
@@ -10,9 +12,9 @@ while True:
         break
 
     frame = cv2.flip(frame, 1)
-    frame = cv2.resize(frame, dsize=(1300, 975))
+    frame = cv2.resize(frame, dsize=(1920, 1080))
     cv2.imshow('mirror', frame)
-    cv2.moveWindow('mirror', 300, 0)
+    cv2.moveWindow('mirror', 0, 0)
     key = cv2.waitKey(1) & 0xFF
     if key == 13:
         break
